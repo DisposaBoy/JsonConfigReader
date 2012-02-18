44694097b5b9ff65e1e84dde15a57d65bf85942b
DisposaBoy <disposaboy@dby.me>
2012-02-18 16:10:07 +0000

    - rewrite

--- reader.go
+++ reader.go
@@ -1,66 +1,91 @@
 package JsonConfigReader
 
 import (
+	"bytes"
 	"io"
 )
 
 type state struct {
-	escapeC   int
-	inString  bool
-	inComment bool
-	r         io.Reader
+	r  io.Reader
+	br *bytes.Reader
+}
+
+func isNL(c byte) bool {
+	return c == '\n' || c == '\r'
+}
+
+func isWS(c byte) bool {
+	return c == ' ' || c == '\t' || isNL(c)
+}
+
+func consumeComment(s []byte, i int) int {
+	if i < len(s) && s[i] == '/' {
+		s[i-1] = ' '
+		for ; i < len(s) && !isNL(s[i]); i += 1 {
+			s[i] = ' '
+		}
+	}
+	return i
+}
+
+func prep(r io.Reader) (s []byte, err error) {
+	buf := &bytes.Buffer{}
+	_, err = io.Copy(buf, r)
+	s = buf.Bytes()
+	if err != nil {
+		return
+	}
+
+	i := 0
+	for i < len(s) {
+		switch s[i] {
+		case '"':
+			i += 1
+			for i < len(s) {
+				if s[i] == '"' {
+					i += 1
+					break
+				} else if s[i] == '\\' {
+					i += 1
+				}
+				i += 1
+			}
+		case '/':
+			i = consumeComment(s, i+1)
+		case ',':
+			j := i
+			for {
+				i += 1
+				if i >= len(s) {
+					break
+				} else if s[i] == '}' || s[i] == ']' {
+					s[j] = ' '
+					break
+				} else if s[i] == '/' {
+					i = consumeComment(s, i+1)
+				} else if !isWS(s[i]) {
+					break
+				}
+			}
+		default:
+			i += 1
+		}
+	}
+	return
 }
 
 // Read acts as a proxy for the underlying reader and cleans p
 // of comments and trailing commas preceeding ] and }
 // comments are delimitted by // up until the end the line
-func (s *state) Read(p []byte) (n int, err error) {
-	n, err = s.r.Read(p)
-	if err == nil {
-		lpcOff := -1
-		var lc, lpc byte
-		for i, c := range p[0:n] {
-			if s.inString {
-				if s.escapeC == 0 {
-					if c == '"' {
-						s.inString = false
-					} else if c == '\\' {
-						s.escapeC += 1
-					}
-				} else {
-					s.escapeC = 0
-				}
-			} else {
-				if s.inComment {
-					if c == '\n' || c == '\r' {
-						s.inComment = false
-					}
-				} else {
-					if lpc == ',' && (c == '}' || c == ']') {
-						p[lpcOff] = ' '
-					}
-					if c != ' ' && c != '\t' && c != '\n' && c != '\r' && c != '/' {
-						lpc = c
-						lpcOff = i
-					}
-
-					if c == '"' {
-						s.inString = true
-					} else if c == '/' && lc == '/' {
-						// don't clear a single /, let a syntax error occur
-						p[i-1] = ' '
-						s.inComment = true
-					}
-				}
-
-				if s.inComment {
-					p[i] = ' '
-				}
-			}
-			lc = c
+func (st *state) Read(p []byte) (n int, err error) {
+	if st.br == nil {
+		var s []byte
+		if s, err = prep(st.r); err != nil {
+			return
 		}
+		st.br = bytes.NewReader(s)
 	}
-	return
+	return st.br.Read(p)
 }
 
 // New returns an io.Reader acting as proxy to r
